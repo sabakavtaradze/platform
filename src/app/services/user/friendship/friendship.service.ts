21047c6c7765b89c7519e0ea22307686a2aa155f
sabakavtaradze <sabakavtaradzee@gmail.com>
2025-11-09 21:16:04 +0400

    fx

--- src/app/services/user/friendship/friendship.service.ts
+++ src/app/services/user/friendship/friendship.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { BaseResponse } from 'src/app/interfaces/ResponseInterface';
+import { BaseResponse} from 'src/app/interfaces/ResponseInterface';
 
 
 
