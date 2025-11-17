67148d1e0c16c825d230b14c3523dcf687c37958
SABA\Sabak <sabakavtaradzee@gmail.com>
2025-11-17 21:56:01 +0400

    f

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,70 +14,36 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 
-import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { AboutpageComponent } from './components/aboutpage/aboutpage.component';
 import { FooterComponent } from './components/mainpages/footer/footer.component';
 import { SharedmoduleModule } from './components/sharedmodule/sharedmodule.module';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 
-@NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, AboutpageComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    RouterModule,
-    MatInputModule,
-    MatFormFieldModule,
-    FormsModule,
-    // MatInputModule,
-    // MatButtonModule,
-    MatIconModule,
-    // MatTreeModule,
-    // MatSelectModule,
-    ReactiveFormsModule,
-    MatDialogModule,
-    MatButtonModule,
-    SharedmoduleModule,
-    // MatIconModule,
-    // MatDialogModule,
-    // MatCheckboxModule,
-    // DragDropModule,
-    // CdkDropList,
-    // MatCardModule,
-    // MdbAccordionModule,
-    // MdbCarouselModule,
-    // MdbCheckboxModule,
-    // MdbCollapseModule,
-    // MdbDropdownModule,
-    // MdbFormsModule,
-    // MdbModalModule,
-    // MdbPopoverModule,
-    // MdbRadioModule,
-    // MdbRangeModule,
-    // MdbRippleModule,
-    // MdbScrollspyModule,
-    // MdbTabsModule,
-    // MdbTooltipModule,
-    // MdbValidationModule,
-    // MatSelectModule,
-    // MatSliderModule,
-    // NgbModule,
-    // NgxBootstrapIconsModule,
-    // MatDatepickerModule,
-    // MatNativeDateModule,
-    HttpClientModule,
-  ],
-
-  bootstrap: [AppComponent],
-  providers: [
-    HttpClient,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true,
-    },
-  ],
-})
+@NgModule({ declarations: [AppComponent, HeaderComponent, FooterComponent, AboutpageComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        RouterModule,
+        MatInputModule,
+        MatFormFieldModule,
+        FormsModule,
+        // MatInputModule,
+        // MatButtonModule,
+        MatIconModule,
+        // MatTreeModule,
+        // MatSelectModule,
+        ReactiveFormsModule,
+        MatDialogModule,
+        MatButtonModule,
+        SharedmoduleModule], providers: [
+        HttpClient,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptor,
+            multi: true,
+        },
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule {}
