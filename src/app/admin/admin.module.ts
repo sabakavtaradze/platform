a2cf619ea7387063757e7c30d0c3471595372fdc
SABA\Sabak <sabakavtaradzee@gmail.com>
2025-11-17 23:11:17 +0400

    finally we fix it

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { AdminPostListComponent } from './admin-post-list/admin-post-list.component';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 
 @NgModule({
   declarations: [AdminComponent, AdminPostListComponent],
-  imports: [CommonModule, AdminRoutingModule, MatButtonModule,],
+  imports: [CommonModule, AdminRoutingModule, MatButtonModule, MatInputModule],
 })
 export class AdminModule { }
--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,8 @@
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
 import { AppModule } from './app/app.module';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideExperimentalDesignSystem()
-  ]
-});
-
-function provideExperimentalDesignSystem(): import("@angular/core").Provider | import("@angular/core").EnvironmentProviders {
-  throw new Error('Function not implemented.');
-}
